5ad04e5a46d7dbad5dd55084421211e7e8956c80
portailp <contact@portailpro.net>
2017-03-20 15:32:52 +0100

    Revert "Upgrade awesomplete + fix multiple autocompletion fields"
    This reverts commit f0a803b75469f01a219703f7f04aa4456632cbc9.

--- inc/awesomplete.js
+++ inc/awesomplete.js
@@ -12,23 +12,26 @@
 
         // Setup
 
-        this.isOpened = false;
-
         this.input = $(input);
-        this.input.setAttribute("autocomplete", "off");
         this.input.setAttribute("aria-autocomplete", "list");
 
         o = o || {};
 
-        configure(this, {
+        configure.call(this, {
             minChars: 2,
             maxItems: 10,
             autoFirst: false,
-            data: _.DATA,
             filter: _.FILTER_CONTAINS,
             sort: _.SORT_BYLENGTH,
-            item: _.ITEM,
-            replace: _.REPLACE
+            item: function (text, input) {
+                return $.create("li", {
+                    innerHTML: text.replace(RegExp($.regExpEscape(input.trim()), "gi"), "<mark>$&</mark>"),
+                    "aria-selected": "false"
+                });
+            },
+            replace: function (text) {
+                this.input.value = text;
+            }
         }, o);
 
         this.index = -1;
@@ -41,7 +44,7 @@
         });
 
         this.ul = $.create("ul", {
-            hidden: "hidden",
+            hidden: "",
             inside: this.container
         });
 
@@ -57,7 +60,7 @@
 
         $.bind(this.input, {
             "input": this.evaluate.bind(this),
-            "blur": this.close.bind(this, { reason: "blur" }),
+            "blur": this.close.bind(this),
             "keydown": function(evt) {
                 var c = evt.keyCode;
 
@@ -69,7 +72,7 @@
                         me.select();
                     }
                     else if (c === 27) { // Esc
-                        me.close({ reason: "esc" });
+                        me.close();
                     }
                     else if (c === 38 || c === 40) { // Down/Up arrow
                         evt.preventDefault();
@@ -79,7 +82,7 @@
             }
         });
 
-        $.bind(this.input.form, {"submit": this.close.bind(this, { reason: "submit" })});
+        $.bind(this.input.form, {"submit": this.close.bind(this)});
 
         $.bind(this.ul, {"mousedown": function(evt) {
             var li = evt.target;
@@ -90,16 +93,15 @@
                     li = li.parentNode;
                 }
 
-                if (li && evt.button === 0) {  // Only select on left click
-                    evt.preventDefault();
-                    me.select(li, evt.target);
+                if (li) {
+                    me.select(li);
                 }
             }
         }});
 
         if (this.input.hasAttribute("list")) {
-            this.list = "#" + this.input.getAttribute("list");
-            this.input.removeAttribute("list");
+            this.list = "#" + input.getAttribute("list");
+            input.removeAttribute("list");
         }
         else {
             this.list = this.input.getAttribute("data-list") || o.list || [];
@@ -120,18 +122,9 @@
                 list = $(list);
 
                 if (list && list.children) {
-                    var items = [];
-                    slice.apply(list.children).forEach(function (el) {
-                        if (!el.disabled) {
-                            var text = el.textContent.trim();
-                            var value = el.value || text;
-                            var label = el.label || text;
-                            if (value !== "") {
-                                items.push({ label: label, value: value });
-                            }
-                        }
+                    this._list = slice.apply(list.children).map(function (el) {
+                        return el.innerHTML.trim();
                     });
-                    this._list = items;
                 }
             }
 
@@ -145,24 +138,18 @@
         },
 
         get opened() {
-            return this.isOpened;
+            return this.ul && this.ul.getAttribute("hidden") == null;
         },
 
-        close: function (o) {
-            if (!this.opened) {
-                return;
-            }
-
+        close: function () {
             this.ul.setAttribute("hidden", "");
-            this.isOpened = false;
             this.index = -1;
 
-            $.fire(this.input, "awesomplete-close", o || {});
+            $.fire(this.input, "awesomplete-close");
         },
 
         open: function () {
             this.ul.removeAttribute("hidden");
-            this.isOpened = true;
 
             if (this.autoFirst && this.index === -1) {
                 this.goto(0);
@@ -173,14 +160,14 @@
 
         next: function () {
             var count = this.ul.children.length;
-            this.goto(this.index < count - 1 ? this.index + 1 : (count ? 0 : -1) );
+
+            this.goto(this.index < count - 1? this.index + 1 : -1);
         },
 
         previous: function () {
             var count = this.ul.children.length;
-            var pos = this.index - 1;
 
-            this.goto(this.selected && pos !== -1 ? pos : count - 1);
+            this.goto(this.selected? this.index - 1 : count - 1);
         },
 
         // Should not be used, highlights specific item without any checks!
@@ -196,37 +183,28 @@
             if (i > -1 && lis.length > 0) {
                 lis[i].setAttribute("aria-selected", "true");
                 this.status.textContent = lis[i].textContent;
-
-                // scroll to highlighted element in case parent's height is fixed
-                this.ul.scrollTop = lis[i].offsetTop - this.ul.clientHeight + lis[i].clientHeight;
-
-                $.fire(this.input, "awesomplete-highlight", {
-                    text: this.suggestions[this.index]
-                });
             }
+
+            $.fire(this.input, "awesomplete-highlight");
         },
 
-        select: function (selected, origin) {
-            if (selected) {
-                this.index = $.siblingIndex(selected);
-            } else {
-                selected = this.ul.children[this.index];
-            }
+        select: function (selected) {
+            selected = selected || this.ul.children[this.index];
 
             if (selected) {
-                var suggestion = this.suggestions[this.index];
+                var prevented;
 
-                var allowed = $.fire(this.input, "awesomplete-select", {
-                    text: suggestion,
-                    origin: origin || selected
+                $.fire(this.input, "awesomplete-select", {
+                    text: selected.textContent,
+                    preventDefault: function () {
+                        prevented = true;
+                    }
                 });
 
-                if (allowed) {
-                    this.replace(suggestion);
-                    this.close({ reason: "select" });
-                    $.fire(this.input, "awesomplete-selectcomplete", {
-                        text: suggestion
-                    });
+                if (!prevented) {
+                    this.replace(selected.textContent);
+                    this.close();
+                    $.fire(this.input, "awesomplete-selectcomplete");
                 }
             }
         },
@@ -240,28 +218,25 @@
                 // Populate list with options that match
                 this.ul.innerHTML = "";
 
-                this.suggestions = this._list
-                    .map(function(item) {
-                        return new Suggestion(me.data(item, value));
-                    })
+                this._list
                     .filter(function(item) {
                         return me.filter(item, value);
                     })
                     .sort(this.sort)
-                    .slice(0, this.maxItems);
+                    .every(function(text, i) {
+                        me.ul.appendChild(me.item(text, value));
 
-                this.suggestions.forEach(function(text) {
-                    me.ul.appendChild(me.item(text, value));
-                });
+                        return i < me.maxItems - 1;
+                    });
 
                 if (this.ul.children.length === 0) {
-                    this.close({ reason: "nomatches" });
+                    this.close();
                 } else {
                     this.open();
                 }
             }
             else {
-                this.close({ reason: "nomatches" });
+                this.close();
             }
         }
     };
@@ -286,58 +261,27 @@
         return a < b? -1 : 1;
     };
 
-    _.ITEM = function (text, input) {
-        var html = input.trim() === '' ? text : text.replace(RegExp($.regExpEscape(input.trim()), "gi"), "<mark>$&</mark>");
-        return $.create("li", {
-            innerHTML: html,
-            "aria-selected": "false"
-        });
-    };
-
-    _.REPLACE = function (text) {
-        this.input.value = text.value;
-    };
-
-    _.DATA = function (item/*, input*/) { return item; };
-
 // Private functions
 
-    function Suggestion(data) {
-        var o = Array.isArray(data)
-            ? { label: data[0], value: data[1] }
-            : typeof data === "object" && "label" in data && "value" in data ? data : { label: data, value: data };
-
-        this.label = o.label || o.value;
-        this.value = o.value;
-    }
-    Object.defineProperty(Suggestion.prototype = Object.create(String.prototype), "length", {
-        get: function() { return this.label.length; }
-    });
-    Suggestion.prototype.toString = Suggestion.prototype.valueOf = function () {
-        return "" + this.label;
-    };
-
-    function configure(instance, properties, o) {
+    function configure(properties, o) {
         for (var i in properties) {
             var initial = properties[i],
-                attrValue = instance.input.getAttribute("data-" + i.toLowerCase());
+                attrValue = this.input.getAttribute("data-" + i.toLowerCase());
 
             if (typeof initial === "number") {
-                instance[i] = parseInt(attrValue);
+                this[i] = +attrValue;
             }
             else if (initial === false) { // Boolean options must be false by default anyway
-                instance[i] = attrValue !== null;
+                this[i] = attrValue !== null;
             }
             else if (initial instanceof Function) {
-                instance[i] = null;
+                this[i] = null;
             }
             else {
-                instance[i] = attrValue;
+                this[i] = attrValue;
             }
 
-            if (!instance[i] && instance[i] !== 0) {
-                instance[i] = (i in o)? o[i] : initial;
-            }
+            this[i] = this[i] || o[i] || initial;
         }
     }
 
@@ -399,29 +343,23 @@
             evt[j] = properties[j];
         }
 
-        return target.dispatchEvent(evt);
+        target.dispatchEvent(evt);
     };
 
     $.regExpEscape = function (s) {
         return s.replace(/[-\\^$*+?.()|[\]{}]/g, "\\$&");
-    };
-
-    $.siblingIndex = function (el) {
-        /* eslint-disable no-cond-assign */
-        for (var i = 0; el = el.previousElementSibling; i++);
-        return i;
-    };
+    }
 
 // Initialization
 
     function init() {
         $$("input.awesomplete").forEach(function (input) {
-            new _(input);
+            new Awesomplete(input);
         });
     }
 
 // Are we in a browser? Check for Document constructor
-    if (typeof Document !== "undefined") {
+    if (typeof Document !== 'undefined') {
         // DOM already loaded?
         if (document.readyState !== "loading") {
             init();
@@ -436,12 +374,12 @@
     _.$$ = $$;
 
 // Make sure to export Awesomplete on self when in a browser
-    if (typeof self !== "undefined") {
+    if (typeof self !== 'undefined') {
         self.Awesomplete = _;
     }
 
 // Expose Awesomplete as a CJS module
-    if (typeof module === "object" && module.exports) {
+    if (typeof exports === 'object') {
         module.exports = _;
     }
 
