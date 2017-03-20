4aadeacdf0b4978fd2dea1a3581c6c9263e06aa2
portailp <contact@portailpro.net>
2017-03-20 15:32:52 +0100

    Revert "Hide expected 'error_log' while running TU (clean PHPUnit log)"
    This reverts commit 0ae5029db56293ee8b6b4a68c49d7ea011fb4676.

--- tests/ApplicationUtilsTest.php
+++ tests/ApplicationUtilsTest.php
@@ -75,12 +75,9 @@ class ApplicationUtilsTest extends PHPUnit_Framework_TestCase
      */
     public function testGetLatestGitVersionCodeInvalidUrl()
     {
-        $oldlog = ini_get('error_log');
-        ini_set('error_log', '/dev/null');
         $this->assertFalse(
             ApplicationUtils::getLatestGitVersionCode('htttp://null.io', 1)
         );
-        ini_set('error_log', $oldlog);
     }
 
     /**
